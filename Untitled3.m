b4887afd13f1b760c320d1b66440e26e52faeb1e
Mayank Khandelwal <mayank.khandelwal@aalto.fi>
2016-12-17 13:56:21 +0200

    Add files via upload

---
+++ Untitled3.m
@@ -0,0 +1,52 @@
+%Loading Data
+load C:\Users\Mayank\Desktop\Ans2\ex2_traindata.txt;
+load C:\Users\Mayank\Desktop\Ans2\ex2_testdata.txt;
+ex2_traindata = sortrows(ex2_traindata);
+
+%Setting Up Data Matrices
+ex2_traindata_x=[];ex2_traindata_y=[];ex2_validdata_x=[];ex2_validdata_y=[];
+ex2_testdata_x=ex2_testdata(:,1);
+ex2_testdata_y=ex2_testdata(:,2);
+
+%Cross Validation Partition
+k=10;
+samplesize = size( ex2_traindata , 1);
+c = cvpartition(samplesize,  'kfold' , k);
+
+%Initialization of Variables
+degrees=15;
+total = zeros(degrees,1);
+p{1} = zeros(2,1);
+err = zeros(degrees,1);
+
+%Obtaining Test Sets
+for i=1 : k
+   trainInd = find(training(c,i) );
+   valInd  = find(test(c,i)       );
+
+%Splitting Training Set
+    for x = trainInd
+    ex2_traindata_x = [ex2_traindata_x ; ex2_traindata(x,1)];
+    ex2_traindata_y = [ex2_traindata_y ; ex2_traindata(x,2)];
+    end
+
+%Splitting Validation Set
+    for y = valInd
+    ex2_validdata_x = [ex2_validdata_x ; ex2_traindata(y,1)];
+    ex2_validdata_y = [ex2_validdata_y ; ex2_traindata(y,2)];
+    end
+
+ %Fitting, Error w.r.t Validation and Test Data Success
+    for counter=1 : degrees  
+    p{counter}=polyfit(ex2_traindata_x,ex2_traindata_y,counter);
+    err(counter,1) = sum((ex2_validdata_y-polyval(p{counter},ex2_validdata_x)).^2)/size(ex2_validdata_x,1);
+    total(counter,1) = total(counter,1)+sum((ex2_testdata_y-polyval(p{counter},ex2_testdata_x)).^2)/size(ex2_testdata_x,1);
+   end   
+end
+
+%Averaging Results
+for j=1 : degrees
+   total(j,1) = total(j,1)/degrees;
+end
+%Maintaining Precision
+vpa(total)
